7bb6f61f40bc23f0fd0d0d323a3788475113c8e8
suivo2001 <suivo178@gmail.com>
2021-11-15 22:19:04 -0500

    feat: add pdf guides

--- src/pages/PatientGuide.js
+++ src/pages/PatientGuide.js
@@ -1,6 +1,11 @@
 import React from 'react'
 
 import Background from '../images/doctor-background.png'
+import EnglishPdf from '../Patient-Guides/Patient-Guide-in-English.pdf'
+import FrenchPdf from '../Patient-Guides/Patient-Guide-in-French.pdf'
+import SpanishPdf from '../Patient-Guides/Patient-Guide-in-Spanish.pdf'
+import ChineseTradPdf from '../Patient-Guides/Patient-Guide-in-Chinese-Traditional.pdf'
+import ChineseSimpPdf from '../Patient-Guides/Patient-Guide-in-Chinese-Simplified.pdf'
 
 const PatientGuide = () => {
     return (
@@ -12,11 +17,11 @@ const PatientGuide = () => {
                 <div className='container is-flex is-justify-content-center'>
                     <div>
                         <p className='content is-flex is-justify-content-center'>We have developed a patient guide for medical access that has been reviewed and validated by academic physicians and health researchers, available in:</p>
-                        <a className='content is-flex is-justify-content-center'>- English</a>
-                        <a className='content is-flex is-justify-content-center'>- Español | Spanish</a>
-                        <a className='content is-flex is-justify-content-center'>- 中文 (简体) | Chinese (Simplified)</a>
-                        <a className='content is-flex is-justify-content-center'>- 中文 (繁體) | Chinese (Traditional)</a>
-                        <a className='content is-flex is-justify-content-center'>- Français | French</a>
+                        <a className='content is-flex is-justify-content-center' href={EnglishPdf} target='_blank' rel='noopener noreferrer'>- English</a>
+                        <a className='content is-flex is-justify-content-center' href={SpanishPdf} target='_blank' rel='noopener noreferrer'>- Español | Spanish</a>
+                        <a className='content is-flex is-justify-content-center' href={ChineseSimpPdf} target='_blank' rel='noopener noreferrer'>- 中文 (简体) | Chinese (Simplified)</a>
+                        <a className='content is-flex is-justify-content-center' href={ChineseTradPdf} target='_blank' rel='noopener noreferrer'>- 中文 (繁體) | Chinese (Traditional)</a>
+                        <a className='content is-flex is-justify-content-center' href={FrenchPdf} target='_blank' rel='noopener noreferrer'>- Français | French</a>
                         <p className='content is-flex is-justify-content-center'>We are currently translating this guide into Hindi, Urdu, French, Vietnamese, and Farsi.</p>
                     </div>
                 </div>

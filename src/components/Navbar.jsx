02f46230a7e1f2fb5ac71b86a9173f07f118e3bb
suivo2001 <suivo178@gmail.com>
2021-11-15 21:20:09 -0500

    feat: collapsible navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import GoogleTranslate from './GoogleTranslate'
 
 const Navbar = () => {
+
+    const [isActive, setIsActive] = useState(false)
+
+    const setActive = () => {
+        const burgerIcon = document.querySelector('#burger')
+        const navbarMenu = document.querySelector('#nav-links')
+        navbarMenu.classList.toggle('is-active')
+    }
+
     return (
         <nav className="navbar is-light px-6">
             <div className="navbar-brand">
                 <a className="navbar-item is-size-3" href='/'>
                     <b>MLAP</b>
                 </a>
+                <GoogleTranslate />
+                
+                <a role="button" className="navbar-burger" id='burger' aria-label="menu" aria-expanded="false" onClick={setActive}>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
             </div>
-
-            <div className="navbar-menu">
+            
+            <div className="navbar-menu" id='nav-links'>
                 <div className='navbar-start'>
                     <a className='navbar-item mx-5' href='/'><b>Medical Literacy Access Project</b></a>
-                    <GoogleTranslate/>
+                    
                 </div>
-                
+
                 <div className="navbar-end">
                     <a className="navbar-item" href='/patient-guide'>
                         Patient Guides
@@ -36,7 +52,7 @@ const Navbar = () => {
                         </a>
                     </div>
                 </div>
-                
+
             </div>
         </nav>
     )

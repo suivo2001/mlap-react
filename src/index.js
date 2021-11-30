24a306eeed0a21f7ade54c35533f32be6ca4365f
Ivo Su <73476329+suivo2001@users.noreply.github.com>
2021-11-29 22:39:22 -0500

    fix: redirect to index.html

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 import 'bulma/css/bulma.min.css';
 import './App.css'
 
 ReactDOM.render(
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
    <App />
   </Router>,
   document.getElementById('root')

31731f2168ecf0d564147a8b19ff29b158241bee
Ivo Su <73476329+suivo2001@users.noreply.github.com>
2022-01-27 12:50:45 -0500

    feat: clinic finder error strip and expand search

--- src/pages/ClinicFinder.js
+++ src/pages/ClinicFinder.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react'
-import {getDistance} from 'geolib'
+import { getDistance } from 'geolib'
 import Geocode from "react-geocode";
 
 import ClinicsJSON from '../spreadsheets/clinics.json'
@@ -25,10 +25,10 @@ const style = {
 }
 
 const castZip = (zipCode) => {
-    if (zipCode.length===3)
-        return "00"+zipCode
-    else if (zipCode.length===4)
-        return "0"+zipCode
+    if (zipCode.length === 3)
+        return "00" + zipCode
+    else if (zipCode.length === 4)
+        return "0" + zipCode
     else
         return zipCode.substring(0, 5)
 }
@@ -42,7 +42,9 @@ const ClinicFinder = (props) => {
     const [currentZip, setCurrentZip] = useState('')
     const [clinicsArray, setClinicsArray] = useState([])
 
-    const searchWithin = 10000
+    const [searchWithin, setSearchWithin] = useState(9000)
+
+    const [showWarning, setShowWarning] = useState(false)
 
     useEffect(() => {
         console.log(ClinicsJSON)
@@ -64,12 +66,12 @@ const ClinicFinder = (props) => {
 
     const handleSubmit = async () => {
 
-        if (ZipDictionary[currentZip]!==undefined) {
+        if (ZipDictionary[currentZip] !== undefined) {
             let newArr = []
             ClinicsJSON.forEach((clinic) => {
                 const compareZip = castZip(clinic[8])
-                
-                if (ZipDictionary[compareZip]!==undefined && getDistance(ZipDictionary[currentZip], ZipDictionary[compareZip])<searchWithin) {
+
+                if (ZipDictionary[compareZip] !== undefined && getDistance(ZipDictionary[currentZip], ZipDictionary[compareZip]) < searchWithin) {
                     Geocode.fromAddress(clinic[1]).then(
                         (response) => {
                             const position = response.results[0].geometry.location;
@@ -85,7 +87,13 @@ const ClinicFinder = (props) => {
             })
             await timeout(1000)
             setClinicsArray(newArr)
+            if (newArr.length===0)
+                setShowWarning(true)
+            else
+                setShowWarning(false)
         }
+        else
+            setShowWarning(true)
 
     }
 
@@ -94,17 +102,32 @@ const ClinicFinder = (props) => {
             <div className='section' id="grad">
                 <h1 className='title is-flex is-justify-content-center has-text-centered has-text-white mx-6'>Find a nearby clinic with your Zip Code!</h1>
             </div>
+            {showWarning?
+                <div className='section is-flex is-justify-content-center has-background-warning'>
+                    <h1 className='content is-flex is-justify-content-center has-text-centered mx-6'>The zip code you entered may not be valid or there may not be a clinic close enough to your location. Try expanding your search area or enter a new zip code.</h1>
+                </div>
+                :''
+            }
+            
 
             <div className='section is-flex is-justify-content-center'>
                 <div className='control'>
                     <label className="label">Zip Code</label>
                     <input className="input" type="text" placeholder="ZIP Code" onChange={(e) => setCurrentZip(e.target.value)} />
+                    <label className="label mt-2">Search within</label>
+                    <div class="select">
+                        <select onChange={(e) => setSearchWithin(e.target.value)}>
+                            <option value={9000}>5 miles</option>
+                            <option value={15*1600}>15 miles</option>
+                            <option value={25*1600}>25 miles</option>
+                        </select>
+                    </div>
                     {/* eslint-disable-next-line */}
-                    <a className="button is-primary mt-3" onClick={handleSubmit}>Search</a>
+                    <a className="button is-primary ml-3" onClick={handleSubmit}>Search</a>
                 </div>
-            
+
             </div>
-            <Map google={window.google} style={style} zoom={clinicsArray.length > 0 ? 11 : 4} onClick={onMapClicked}
+            <Map google={window.google} style={style} zoom={clinicsArray.length > 0 ? 10 : 4} onClick={onMapClicked}
                 initialCenter={{
                     lat: 37.09,
                     lng: -88.081807
@@ -117,9 +140,9 @@ const ClinicFinder = (props) => {
                         key={index}
                         name={clinic[0]}
                         address={clinic[1]}
-                        website = {clinic[2]}
-                        phone = {clinic[3]}
-                        email = {clinic[4]}
+                        website={clinic[2]}
+                        phone={clinic[3]}
+                        email={clinic[4]}
                         position={clinic[clinic.length - 1]} />)
                 })}
 

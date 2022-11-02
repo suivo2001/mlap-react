eba6cf590e765a43a789a93e89bf67cf1f21d158
johnc-lin <60355792+johnc-lin@users.noreply.github.com>
2022-11-01 23:02:57 -0400

    attempt #2, hope this still works

--- src/pages/PatientGuide.js
+++ src/pages/PatientGuide.js
@@ -17,11 +17,13 @@ const PatientGuide = () => {
                 <div className='container is-flex is-justify-content-center'>
                     <div>
                         <p className='content is-flex is-justify-content-center'>We have developed a patient guide for medical access that has been reviewed and validated by academic physicians and health researchers, available in:</p>
-                        <a className='content is-flex is-justify-content-center' href={EnglishPdf} target='_blank' rel='noopener noreferrer'>- English</a>
-                        <a className='content is-flex is-justify-content-center' href={SpanishPdf} target='_blank' rel='noopener noreferrer'>- Español | Spanish</a>
-                        <a className='content is-flex is-justify-content-center' href={ChineseSimpPdf} target='_blank' rel='noopener noreferrer'>- 中文 (简体) | Chinese (Simplified)</a>
-                        <a className='content is-flex is-justify-content-center' href={ChineseTradPdf} target='_blank' rel='noopener noreferrer'>- 中文 (繁體) | Chinese (Traditional)</a>
-                        <a className='content is-flex is-justify-content-center' href={FrenchPdf} target='_blank' rel='noopener noreferrer'>- Français | French</a>
+                        <p className='content mx-6 my-3'>
+                            <ul><a href={EnglishPdf} target='_blank' rel='noopener noreferrer'>English</a></ul>
+                            <ul><a href={SpanishPdf} target='_blank' rel='noopener noreferrer'>Español | Spanish</a></ul>
+                            <ul><a href={ChineseSimpPdf} target='_blank' rel='noopener noreferrer'>中文 (简体) | Chinese (Simplified)</a></ul>
+                            <ul><a href={ChineseTradPdf} target='_blank' rel='noopener noreferrer'>中文 (繁體) | Chinese (Traditional)</a></ul>
+                            <ul><a href={FrenchPdf} target='_blank' rel='noopener noreferrer'>Français | French</a></ul>
+                        </p>
                         <p className='content is-flex is-justify-content-center'>Our guides were written and revised in accordance with the guidelines and criteria of the International Patient Decision Aid Standards Collaboration.</p>
                     </div>
                 </div>

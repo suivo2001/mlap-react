c35abaca84a975b9e48232bcd53fe2b534b9b0c2
johnc-lin <60355792+johnc-lin@users.noreply.github.com>
2022-11-01 21:18:30 -0400

    updated

--- src/pages/Governance.js
+++ src/pages/Governance.js
@@ -12,8 +12,8 @@ const Governance = () => {
                 <div className='container is-flex is-justify-content-center '>
                     <div>
                         <p className='content is-flex is-justify-content-center'>Our directors meet virtually each month to discuss maintenance of existing consumer health resources and ongoing efforts to translate additional resources and expand community outreach, and additionally on an ad hoc basis. We also solicit advice from our advisors on an ongoing basis for projects in their area of specialty.</p>
-                        <p className='content is-flex is-justify-content-center'>We are a nonprofit project, not a nonprofit organization. Based on advice from the Swearer Center from Public Service and the Rhode Island Foundation, we decided against incorporation. As a nonprofit project, we are fully able to achieve all of our goals, and we hold ourselves to the same standard of transparency and accountability expected of any nonprofit organization.</p>
-                        <p className='content is-flex is-justify-content-center'>Please <a href='/contact'>contact us</a> if you have any order of business that you would like the board to discuss.</p>
+                        <p className='content is-flex is-justify-content-center'>We are a nonprofit project, not a nonprofit organization. Based on advice from the Swearer Center for Public Service and the Rhode Island Foundation, we decided against incorporation. As a nonprofit project, we are fully able to achieve all of our goals, and we hold ourselves to the same standard of transparency and accountability expected of any nonprofit organization.</p>
+                        <p className='content is-flex is-justify-content-center'>Please <a href='/contact'> contact us </a> if you have any order of business that you would like the board to discuss.</p>
                     </div>
                 </div>
             </div>

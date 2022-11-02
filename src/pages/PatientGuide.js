f78507ed204bd2d16df5f56ebdced224181a41e3
johnc-lin <60355792+johnc-lin@users.noreply.github.com>
2022-11-01 22:57:27 -0400

    restore to original, see if this works better

--- src/pages/PatientGuide.js
+++ src/pages/PatientGuide.js
@@ -1,5 +1,10 @@
 import React from 'react'
 
+import EnglishPdf from '../Patient-Guides/Patient-Guide-in-English.pdf'
+import FrenchPdf from '../Patient-Guides/Patient-Guide-in-French.pdf'
+import SpanishPdf from '../Patient-Guides/Patient-Guide-in-Spanish.pdf'
+import ChineseTradPdf from '../Patient-Guides/Patient-Guide-in-Chinese-Traditional.pdf'
+import ChineseSimpPdf from '../Patient-Guides/Patient-Guide-in-Chinese-Simplified.pdf'
 import Background from '../images/doctor-background.png'
 
 const PatientGuide = () => {
@@ -11,9 +16,13 @@ const PatientGuide = () => {
             <div className='section'>
                 <div className='container is-flex is-justify-content-center'>
                     <div>
-                        <p className='content has-text-centered mx-6'>We have developed a patient guide for medical access that has been reviewed and validated by academic physicians and health researchers, available in <a href="../Patient-Guides/Patient-Guide-in-English.pdf">English</a>, <a href="../Patient-Guides/Patient-Guide-in-Spanish.pdf">Español | Spanish</a>, <a href="../Patient-Guides/Patient-Guide-in-Chinese-Simplified.pdf">中文 (简体) | Chinese (Simplified)</a>, <a href="../Patient-Guides/Patient-Guide-in-Chinese-Traditional.pdf">中文 (繁體) | Chinese (Traditional)</a>, and <a href="../Patient-Guides/Patient-Guide-in-French.pdf">Français | French</a>.</p>
-                        <p className='content has-text-centered mx-6'>Our guides were written and revised in accordance with the guidelines and criteria of the International Patient Decision Aid Standards Collaboration.</p>
-                        <iframe src="https://drive.google.com/file/d/1lkrrHV_4Q02O-sWj_zMXtLEXQdATGGBh/preview" width="640" height="480" allow="autoplay"></iframe>
+                        <p className='content is-flex is-justify-content-center'>We have developed a patient guide for medical access that has been reviewed and validated by academic physicians and health researchers, available in:</p>
+                        <a className='content is-flex is-justify-content-center' href={EnglishPdf} target='_blank' rel='noopener noreferrer'>- English</a>
+                        <a className='content is-flex is-justify-content-center' href={SpanishPdf} target='_blank' rel='noopener noreferrer'>- Español | Spanish</a>
+                        <a className='content is-flex is-justify-content-center' href={ChineseSimpPdf} target='_blank' rel='noopener noreferrer'>- 中文 (简体) | Chinese (Simplified)</a>
+                        <a className='content is-flex is-justify-content-center' href={ChineseTradPdf} target='_blank' rel='noopener noreferrer'>- 中文 (繁體) | Chinese (Traditional)</a>
+                        <a className='content is-flex is-justify-content-center' href={FrenchPdf} target='_blank' rel='noopener noreferrer'>- Français | French</a>
+                        <p className='content is-flex is-justify-content-center'>Our guides were written and revised in accordance with the guidelines and criteria of the International Patient Decision Aid Standards Collaboration.</p>
                     </div>
                 </div>
             </div>
